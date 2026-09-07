846f3af676aee73169c3af909ead8733a5b46543
Sarah949 <Sarah-Almahfoudh@hotmail.com>
2026-09-07 14:18:50 +0300

    first commit

---
+++ rock_paper_scissors.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+void main() {
+  stdout.write('Rock, paper or scissors? (r/p/s) ');
+  final input = stdin.readLineSync();
+  print(input);
+// while true
+  // show prompt
+  // read user input from console
+  // If the user enters "r", "p", or "s", treat this as a valid move
+  // CHOOSE the ai move at random
+  // compare the player move with the ai move
+  // show the result
+  // else If the user enters "q", quit the program.
+  //If the user enters anything else, show "Invalid input", and prompt again.
+}
