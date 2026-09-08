592b16081ce7f32fdfba333dcbaf62f10f3f13d9
Sarah949 <Sarah-Almahfoudh@hotmail.com>
2026-09-08 13:44:31 +0300

    add while loop

--- rock_paper_scissors.dart
+++ rock_paper_scissors.dart
@@ -1,9 +1,18 @@
 import 'dart:io';
 
 void main() {
-  stdout.write('Rock, paper or scissors? (r/p/s) ');
-  final input = stdin.readLineSync();
-  print(input);
+  while (true) {
+    stdout.write('Rock, paper or scissors? (r/p/s) ');
+    final input = stdin.readLineSync();
+    if (input == 'r' || input == 'p' || input == 's') {
+      print('Slelected: $input');
+    } else if (input == 'q') {
+      break;
+    } else {
+      print('Invalid input');
+    }
+  }
+}
 // while true
   // show prompt
   // read user input from console
@@ -13,4 +22,4 @@ void main() {
   // show the result
   // else If the user enters "q", quit the program.
   //If the user enters anything else, show "Invalid input", and prompt again.
-}
+
